25adc22f066e06fcc6c7d0a3f6bde693b1d3b1ec
danielcajueiro <danielcajueiro@gmail.com>
2015-02-21 12:37:01 -0200

    Update and rename lang to lang/caju_catrem_default.php

---
+++ lang/caju_catrem_default.php
@@ -0,0 +1,9 @@
+<?php
+
+
+return array(
+
+	CAJU_remove_cat_module_admin::LANG_ID_ADMIN_PLUGIN_ENABLED => 'Plugin enabled',
+	CAJU_remove_cat_module_admin::LANG_ID_ADMIN_SAVE_SETTINGS_BUTTON => 'Save settings',
+	CAJU_remove_cat_module_admin::LANG_ID_ADMIN_SETTINGS_SAVED => 'Settings saved',
+);
