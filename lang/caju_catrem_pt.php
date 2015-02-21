2253d4d8556963b21e4aeb2080655bb8904e16d7
danielcajueiro <danielcajueiro@gmail.com>
2015-02-21 12:36:21 -0200

    Create caju_catrem_pt.php

---
+++ lang/caju_catrem_pt.php
@@ -0,0 +1,12 @@
+<?php
+
+
+
+return array(
+
+	CAJU_remove_cat_module_admin::LANG_ID_ADMIN_PLUGIN_ENABLED => 'Plugin habilitado',
+	CAJU_remove_cat_module_admin::LANG_ID_ADMIN_SAVE_SETTINGS_BUTTON => 'Configurações salvas',
+	CAJU_remove_cat_module_admin::LANG_ID_ADMIN_SETTINGS_SAVED => 'Configurações salvas',
+
+
+);
